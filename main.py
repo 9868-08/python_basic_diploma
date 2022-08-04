640e8c8e2e6ffd3b372226c4dbb212eb71a26cb3
Роман Ермолаев <9868.08@gmail.com>
2022-08-04 23:09:53 +0400

    bot worked base commands save/load history to json in process

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import os
 import configparser
 import telebot
 import requests
+from datetime import datetime
+import json
+
+
+
+class My_json():
+    def __init__(self):
+        self.datetime = str()
+        self.command = str()
+
+
+def write_log(logfile, to_json):
+    with open(logfile, 'w') as f:
+        f.write(json.dumps(to_json))
+
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-TOKEN=config.get("DEFAULT", "TOKEN")
-user_id=config.get("DEFAULT", "user_id")
+TOKEN = config.get("DEFAULT", "TOKEN")
+user_id = config.get("DEFAULT", "user_id")
 print(TOKEN)
 
 bot = telebot.TeleBot(TOKEN)
@@ -15,10 +31,11 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "help":
-        bot.send_message(message.from_user.id, " You can use commands:\n help\n lowprice\n highprice\n bestdeal\n history")
+        bot.send_message(message.from_user.id,
+                         " You can use commands:\n help\n lowprice\n highprice\n bestdeal\n history")
     elif message.text == "lowprice":
         bot.send_message(message.from_user.id, "you input - lowprice")
-        #bot.send_message(message.from_user.id, lowprice())
+        # bot.send_message(message.from_user.id, lowprice())
     elif message.text == "highprice":
         bot.send_message(message.from_user.id, "you input - highprice")
     elif message.text == "bestdeal":
