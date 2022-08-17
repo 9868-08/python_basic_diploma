c3225bfab06736b76981a211ea108ca6dda93f0b
Роман Ермолаев <9868.08@gmail.com>
2022-08-17 22:40:08 +0400

    city and date input in process

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import os
+# import os
 import configparser
 import telebot
 import requests
-from datetime import datetime
+# from datetime import datetime
+# import json
 import json
+import asyncio
 from datetime import datetime
-import json
-from datetime import datetime
+import handlers
 
 
-class My_json():
+class My_json:
     def __init__(self):
         self.datetime = str()
         self.command = str()
@@ -17,15 +18,14 @@ class My_json():
     def __str__(self):
         return str(self.datetime) + "\t\t" + str(self.command)
 
-    def append(self, str_to_append):
+    @staticmethod
+    def append(str_to_append):
         now = datetime.now()  # current date and time
         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
         to_append_dict = dict()
         to_append_dict[date_time] = str_to_append
-        print(to_append_dict)
         with open(logfile, 'a') as f:
-            print("to_append_dict=", to_append_dict, "logfile=", logfile)
-            json.dump(to_append_dict, logfile, indent=4)  # сериализация JSON
+            json.dump(to_append_dict, f, indent=4)  # сериализация JSON
         f.close()
 
 
@@ -34,11 +34,37 @@ def append(str_to_append):
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
     to_append_dict = dict()
     to_append_dict[date_time] = str_to_append
-    print(to_append_dict)
+    #    print(to_append_dict)
     with open(logfile, 'a') as f:
         print("to_append_dict=", to_append_dict, "logfile=", logfile)
         json.dump(to_append_dict, f, indent=4)  # сериализация JSON
     f.close()
+
+
+'''def hotels_requiest():          # запрос списка всех отелей без сортировки
+    url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+    querystring = {"query": "new york", "locale": "en_US", "currency": "RUB"}
+    headers = {
+        "X-RapidAPI-Key": "a66df32f87mshe86994164d3c458p18029djsnb52f634dfcfe",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    #    print(response.text)
+    return response.text'''
+
+
+def hotels_requiest_highprice(def_city, count):  # запрос списка всех отелей c сортировкой по цене сперва дорогие
+    url = "https://hotels4.p.rapidapi.com/properties/list"
+    querystring = {"destinationId": "1506246", "pageNumber": "1", "pageSize": "25", "checkIn": "<REQUIRED>",
+                   "checkOut": "<REQUIRED>", "adults1": "1", "sortOrder": "PRICE_HIGHEST_FIRST", "locale": "en_US",
+                   "currency": "USD"}
+    headers = {
+        "X-RapidAPI-Key": "a66df32f87mshe86994164d3c458p18029djsnb52f634dfcfe",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
+    return response.text
 
 
 my_json = My_json()
@@ -49,16 +75,16 @@ TOKEN = config.get("DEFAULT", "TOKEN")
 user_id = config.get("DEFAULT", "user_id")
 bot = telebot.TeleBot(TOKEN)
 
-
-@bot.message_handler(content_types=['text'])
+'''@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "help":
-        bot.send_message(message.from_user.id,
+       bot.send_message(message.from_user.id,
                          " You can use commands:\n help\n lowprice\n highprice\n bestdeal\n history")
     elif message.text == "lowprice":
         bot.send_message(message.from_user.id, "you input - lowprice")
-        my_json.append("lowprice")
-        bot.send_message(message.from_user.id, "you input - lowprice 2")
+        all_hotels_str = hotels_requiest()
+        all_hotels_dict = eval(all_hotels_str)
+        my_json.append(("lowprice command", all_hotels_dict))
     elif message.text == "highprice":
         append("highprice")
         bot.send_message(message.from_user.id, "you input - highprice")
@@ -68,6 +94,39 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "you input - history")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю.")
+'''
 
 
-bot.polling(none_stop=True, interval=5)
+@bot.message_handler(Command(['lowprice', 'highprice', 'bestdeal']), state='*')
+async def define_state(message: Message, state: FSMContext):
+    """"Catches lowprice and higprice commands. Asks user for city name"""
+
+    command = message.text.lstrip('/')
+    await send_city_request_with_photo(message, command)
+    await SelectCity.wait_city_name.set()
+
+    await register_command_in_db(command, message, state)
+
+
+
+@bot.message_handler(content_types=['text'])
+def main(message):
+    bot.send_message(message.from_user.id, "В каком городе ищем отель?")
+    city = message.text
+    bot.send_message(message.from_user.id, "ищем отель в городе: " + city)
+    city = "New   york"
+    bot.send_message(message.from_user.id, "для отладки ищем отель в городе: " + city)
+    return city
+
+    bot.send_message(message.from_user.id, "The check-in date at hotel, formated as yyyy-MM-dd:")
+    checkIn = message.text
+    bot.send_message(message.from_user.id, "The check-out date at hotel, formated as yyyy-MM-dd:")
+    checkOut = message.text
+    bot.send_message(message.from_user.id, "Выбран город " + city + " с " + checkIn + "по" + checkOut)
+
+
+
+bot.send_message(message.from_user.id, "you input - highprice")
+bot.polling(none_stop=False, interval=5)
+bot.send_message(message.from_user.id, "для отладки ищем отель в городе: " + city)
+
--- test.py
+++ test.py
@@ -1,40 +1,38 @@
-from datetime import datetime
+# import os
+import configparser
+import telebot
+import requests
 import json
-from datetime import datetime
+import asyncio
 
 
-class My_json():
-    def __init__(self):
-        self.datetime = str()
-        self.command = str()
-
-    def __str__(self):
-        return str(self.datetime) + "\t\t" + str(self.command)
-
-    def append(self, json_file, str_to_append):
-        with open(logfile, 'w') as f:
-            json.dump(str_to_append, indent=4)  # сериализация JSON
-#            f.write(json.dumps(my_json))
-        f.close()
-
-
-my_json = My_json()
 logfile = "diploma_log.json"
-
-str_to_append = dict()
-
-now = datetime.now() # current date and time
-date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-#print("date and time:",date_time)
-
-command = "lowprice"
-
-str_to_append = {'08/07/2022, 17:44:03': 'highprice'}
+config = configparser.ConfigParser()
+config.read('config.ini')
+TOKEN = config.get("DEFAULT", "TOKEN")
+user_id = config.get("DEFAULT", "user_id")
+bot = telebot.TeleBot(TOKEN)
 
 
-print(str_to_append)
+@bot.message_handler(content_types=['text'])
+async def show_info(message):
+    bot.send_message(message.chat.id, 'Итоговый проект курса «Python Basic» Ермолаева Романа')
+    bot.send_message(message.from_user.id, "В каком городе ищем отель?")
+    return
 
-with open(logfile, 'a') as f:
-    json.dump(str_to_append, f, indent=4)  # сериализация JSON
 
-f.close()
+@bot.message_handler(content_types=['text'])
+async def get_city(message):
+    await asyncio.sleep(10)
+    city = message.text
+    return city
+
+
+@bot.message_handler(content_types=['text'])
+async def get_city(message):
+    await asyncio.sleep(20)
+    city = message.text
+    bot.send_message(message.from_user.id, "ищем отель в городе: " + city)
+
+
+bot.polling(none_stop=True, interval=5)
