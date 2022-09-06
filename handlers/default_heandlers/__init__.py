7431f335b12cfd84afecd52fc5ce809b2e5b170d
9868.08@gmail.com <Mis8FhXJ>
2022-09-06 12:19:58 +0400

    diploma in work

--- handlers/default_heandlers/__init__.py
+++ handlers/default_heandlers/__init__.py
@@ -1,4 +1,2 @@
 #from . import start
-from . import help
-from . import lowprice
 from . import echo
--- loader.py
+++ loader.py
@@ -1,8 +1,8 @@
 from telebot import TeleBot
-from telebot.storage import StateMemoryStorage
+#from telebot.storage import StateMemoryStorage
 
 from config_data import config
 
-storage = StateMemoryStorage()
-bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
+#storage = StateMemoryStorage()
+bot = TeleBot(config.BOT_TOKEN)
 
