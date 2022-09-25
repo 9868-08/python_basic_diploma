802c9844c78e01d28cd8dd91bae253c1ec22fbd4
Ермолаев Роман Евгеньевич <REErmolaev@sberbank.ru>
2022-09-25 15:29:18 +0400

    mongodb server - 130.61.88.150

--- database/connect_to_db/client.py
+++ database/connect_to_db/client.py
@@ -3,8 +3,11 @@ from motor.motor_asyncio import AsyncIOMotorCollection
 
 from config_data.config import MONGO_DB_USERNAME, MONGO_DB_PASSWORD
 
-client = motor_asyncio.AsyncIOMotorClient(f'mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@hotelscluster.wodas.mongodb.net'
+# mongoserver.db = 130.61.88.150 (/etc/hosts)
+client = motor_asyncio.AsyncIOMotorClient(f'mongodb://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@130.61.88.150'
                                           '/?retryWrites=true&w=majority')
+#client = motor_asyncio.AsyncIOMotorClient(f'mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@hotelscluster.wodas.mongodb.net'
+#                                          '/?retryWrites=true&w=majority')
 
 
 def get_favorites_collection() -> AsyncIOMotorCollection:
