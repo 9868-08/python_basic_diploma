aa5c9f2f8683deb4145df1327a0d9f49b440b077
Ермолаев Роман Евгеньевич <REErmolaev@sberbank.ru>
2023-01-19 08:52:24 +0400

    хендлер ловящий highprice

--- handlers/action_handlers/city_requiest.py
+++ handlers/action_handlers/city_requiest.py
@@ -1,84 +1,27 @@
-import telebot # telebot
-
-from telebot import custom_filters
-from telebot.handler_backends import State, StatesGroup #States
-
-# States storage
+import telebot  # telebot
+from states import bot_states
+from telebot import TeleBot, custom_filters
+from telebot.handler_backends import State, StatesGroup  # States
 from telebot.storage import StateMemoryStorage
-
-state_storage = StateMemoryStorage() # you can init here another storage
-
+from config_data import config
+from states import bot_states
 from loader import bot
 
-# States group.
-class MyStates(StatesGroup):
-    # Just name variables differently
-    city = State() # creating instances of State class is enough from now
-    date = State()
-    age = State()
-    result = State()
+
+@bot.message_handler(commands=['lowprice', 'highprice', 'bestdeal'])
+def city_requiest (user_id=message.from_user, chat_id=message.chat_id):
+    global mycity
+    mycity = ""
+    bot.set_state(message.from_user.id,bot_states.MyStates.far_away_from_center, message.chat.id)
+    bot.send_message(message.chat.id, 'Hi, write me a target city')
+    mycity = message.text
+    print("city=", mycity)
 
 
-
-
-@bot.message_handler(commands=['start'])
-def start_ex(message):
-    """
-    Start command. Here we are starting state
-    """
-    bot.set_state(message.from_user.id, MyStates.city, message.chat.id)
-    bot.send_message(message.chat.id, 'Hi, write me a city')
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['city'] = message.text
-    print('bot.get_state(message)', bot.get_state(message))
- 
-
-# Any state
-@bot.message_handler(state="*", commands=['cancel'])
-def any_state(message):
-    """
-    Cancel state
-    """
-    bot.send_message(message.chat.id, "Your state was cancelled.")
+@bot.message_handler(state=bot_states.MyStates.far_away_from_center)
+def name_get(message):
+    bot.send_message(message.chat.id, 'How far away from center?')
     bot.delete_state(message.from_user.id, message.chat.id)
-    print('bot.get_state(message)', bot.get_state(message))
-
-
-@bot.message_handler(state=MyStates.city)
-def date_get(message):
-    """
-    State 1. Will process when user's state is MyStates.city.
-    """
-    bot.send_message(message.chat.id, 'Now write me a date')
-    bot.set_state(message.from_user.id, MyStates.date, message.chat.id)
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['city'] = message.text
-    print('bot.get_state(message)', bot.get_state(message))
- 
+        data['name'] = message.text
 
-# result
-@bot.message_handler(state=MyStates.date)
-def set_date(message):
-    bot.set_state(message.from_user.id, MyStates.result, message.chat.id)
-    print(MyStates)
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['date'] = message.text
-    print('bot.get_state(message)', bot.get_state(message))
-
-@bot.message_handler(state=MyStates.result)
-def ready_for_answer(message):
-    print ("ready_for_answer(message):")
-    msg = ("Ready, take a look:\n<b>"
-           f"City: {data['city']}\n"
-            f"Date: {data['date']}\n")
-    bot.send_message(message.chat.id, msg, parse_mode="html")
-    bot.delete_state(message.from_user.id, message.chat.id)
-    print('bot.get_state(message)', bot.get_state(message))
-
-
-# register filters
-
-bot.add_custom_filter(custom_filters.StateFilter(bot))
-bot.add_custom_filter(custom_filters.IsDigitFilter())
-
-print("city_requiest.py")
--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 from loader import bot
 import handlers
 from utils.set_bot_commands import set_default_commands
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-import logging
-
-
-def on_startup(dispatcher):
-    logger = logging.getLogger(__name__)
-
-    dispatcher.setup_middleware(LoggingMiddleware())
-    logger.info("Starting bot")
-    logging.basicConfig(
-        level=logging.INFO,
-        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
-    )
-
-    set_default_commands(dispatcher)
-
 
 if __name__ == '__main__':
-    bot.infinity_polling(skip_pending=True)
-    #executor.start_polling(dp, on_startup=on_startup)
-
+    set_default_commands(bot)
+    bot.infinity_polling()
