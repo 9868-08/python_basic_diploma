bf73646a82ff1360eec40e79e6a6bd63c5d4968f
oman <9868.08@gmail.com>
2023-03-23 22:09:28 +0400

    api_request

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from loader import bot
-from utils.set_bot_commands import set_default_commands
+#from utils.set_bot_commands import set_default_commands
 from telebot import custom_filters
 import handlers
 
@@ -7,5 +7,5 @@ bot.add_custom_filter(custom_filters.StateFilter(bot))
 bot.add_custom_filter(custom_filters.IsDigitFilter())
 
 if __name__ == '__main__':
-    set_default_commands(bot)
+#    set_default_commands(bot)
     bot.infinity_polling(skip_pending=True)
