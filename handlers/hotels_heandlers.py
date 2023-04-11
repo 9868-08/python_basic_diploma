9045ac8ed1ce3246edb6737bd37b495e68e978b4
Ермолаев Роман Евгеньевич <REErmolaev@sberbank.ru>
2023-04-11 16:32:23 +0400

    selected command saved in data['selected_command']

--- handlers/hotels_heandlers.py
+++ handlers/hotels_heandlers.py
@@ -1,21 +1,26 @@
 from loader import bot
 from states import bot_states
 from database import diploma_database
-#from handlers.work_with_hotels_handlers.city_handlers import select_city
+import jsonpickle
+import json
+# from handlers.work_with_hotels_handlers.city_handlers import select_city
 import time
 
 
 @bot.message_handler(commands=['lowprice', 'highprice', 'bestdeal'])
 def start_highprice(message):
+    message_dict = json.loads(jsonpickle.encode(message))
+    data = dict()
+    data['selected_command'] = message_dict['text']
+    print(data['selected_command'])
+    bot.send_message(message.chat.id, data['selected_command'] + ' was selected. \n Hi, write me a city')
+
     diploma_database.Hotel_search('highprice')
     request = diploma_database.Hotel_search
-    bot.send_message(message.chat.id, 'Hi, write me a city')
-    #    bot.set_state(message.from_user.id, bot_states.MyStates.city, message.chat.id)
-    data = dict()
     data['city'] = "Boston"
     data['how_much_hotels'] = 2
     data['need_photos'] = "Y"
     data['how_much_photos'] = 2
-    print(data)
+
     bot.set_state(message.from_user.id, bot_states.MyStates.city, message.chat.id)
     return
