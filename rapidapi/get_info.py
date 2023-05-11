dd1686ca3778f9c14d2a2e9b0ddc9ecabcd3588f
Ермолаев Роман Евгеньевич <REErmolaev@sberbank.ru>
2023-05-11 16:57:02 +0400

    2023_05_11 cosmetic clean. Got location_list. get_info.get_request works. Hotel pictures does not works

--- rapidapi/get_info.py
+++ rapidapi/get_info.py
@@ -39,17 +39,15 @@ def get_request(url, params):
         if response.status_code == codes.ok:
             # return response.json()
             cities = list()
-            # cities_dict = dict()
+            cities_dict = dict()
             response_text = response.text
             response_dict = ast.literal_eval(response_text)
             for i in response_dict['sr']:
                 if 'gaiaId' in i:
-                    cities.append(
-                        dict(id=i['gaiaId'],
-                             region_name=i['regionNames']['fullName'])
-                    )
-            #               continue
-            return cities
+                    cities.append(dict(id=i['gaiaId'], region_name=i['regionNames']['fullName']))
+                    cities_dict[i['gaiaId']] = i['regionNames']['fullName']
+            print('cities_dict= ', cities_dict)
+            return cities_dict
 
     except Exception as e:
         print(e)
@@ -58,11 +56,40 @@ def get_request(url, params):
 
 
 def post_request(method_endswith, params):
-    payload = params
-
+    # payload = params
+    payload = {
+        "currency": "USD",
+        "eapid": 1,
+        "locale": "en_US",
+        "siteId": 300000001,
+        "destination": {"regionId": "6054439"},
+        "checkInDate": {
+            "day": 10,
+            "month": 10,
+            "year": 2022
+        },
+        "checkOutDate": {
+            "day": 15,
+            "month": 10,
+            "year": 2022
+        },
+        "rooms": [
+            {
+                "adults": 2,
+                "children": [{"age": 5}, {"age": 7}]
+            }
+        ],
+        "resultsStartingIndex": 0,
+        "resultsSize": 200,
+        "sort": "PRICE_LOW_TO_HIGH",
+        "filters": {"price": {
+            "max": 150,
+            "min": 100
+        }}
+    }
     headers = {
         "content-type": "application/json",
-        "X-RapidAPI-Key": "7d70182871msh0110c820c62cce9p13613fjsna334f8c7dfe3",
+        "X-RapidAPI-Key": "f11fe670bamshdaffc29f3f33e2bp19bde9jsn5f0726b4228a",
         "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
     }
 
