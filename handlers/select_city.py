9998c7391c68a17a648edc5712394968fb732f6f
Roman <9868.08@gmail.com>
2023-05-13 20:42:24 +0400

    call handle_command_adminwindow does not run

--- handlers/select_city.py
+++ handlers/select_city.py
@@ -3,7 +3,12 @@ from telebot.storage import StateMemoryStorage
 from loader import bot
 import test
 import rapidapi.get_info
-import mk_telegram_inline_keyboard
+import telebot
+import ast
+import time
+from telebot import types
+from states import bot_states
+from loader import bot
 
 state_storage = StateMemoryStorage()
 
@@ -15,8 +20,7 @@ def MyStates_city(message):
 
     location_dict = rapidapi.get_info.api_request('locations/v3/search', {"q": data['city'], "locale": "ru_RU"},
                                                   'GET')  # предоставляет ответ по выбранной локации из которого нужно вытянуть id локации
-    #    location_id = location_json['sr'][0]['gaiaId']
-#    bot.send_message(message.chat.id, " location_dict = " + str(location_dict))
+    handle_command_adminwindow(location_dict)
 
     bot.set_state(message.from_user.id, bot_states.MyStates.how_much_hotels, message.chat.id)
     bot.send_message(message.chat.id, 'Now write how much hotels to search')
@@ -123,3 +127,60 @@ def print_results(message):
         bot.send_photo(message.chat.id, str(item['propertyImage']['image']['url']), caption='фото в отеле '+ item['name'])
         count += 1
     return hotel_id_list
+
+
+def get_location_id(message, location_dict):
+    '''location_dict = [{'id': '660', 'region_name': 'Бостон, Suffolk County, Массачусетс, США'},
+                     {'id': '6340396', 'region_name': 'Даунтаун-Бостон, Бостон, Массачусетс, США'},
+                     {'id': '5459778', 'region_name': 'Бостон, Массачусетс, США (BOS-Логан, международный)'},
+                     {'id': '3000448054', 'region_name': 'Бостон, Нью-Йорк, США'}]'''
+    # print(location_dict)
+    # print(stringList)
+    # stringList = location_dict
+    bot.send_message(message.chat.id, 'location_dic, location_dict = ' + str(location_dict))
+    #    print('def get_location_id')
+    bot.set_state(message.from_user.id, bot_states.MyStates.city_detail, message.chat.id)
+
+
+def makeKeyboard(location_dict):
+    markup = types.InlineKeyboardMarkup()
+
+    for key, value in location_dict.items():
+        markup.add(types.InlineKeyboardButton(text=value,
+                                              callback_data=key))
+        print(key, value)
+
+    return markup
+
+
+# @bot.message_handler(commands=['test'])
+@bot.message_handler(state=bot_states.MyStates.city_detail)
+def handle_command_adminwindow(message, location_dict):
+    bot.send_message(chat_id=message.chat.id,
+                     text="Here are the values of stringList",
+                     reply_markup=makeKeyboard(location_dict),
+                     parse_mode='HTML')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_query(call):
+    if (call.data.startswith("['value'")):
+        print(f"call.data : {call.data} , type : {type(call.data)}")
+        print(
+            f"ast.literal_eval(call.data) : {ast.literal_eval(call.data)} , type : {type(ast.literal_eval(call.data))}")
+        valueFromCallBack = ast.literal_eval(call.data)[1]
+        keyFromCallBack = ast.literal_eval(call.data)[2]
+        bot.answer_callback_query(callback_query_id=call.id,
+                                  show_alert=True,
+                                  text="You Clicked " + valueFromCallBack + " and key is " + keyFromCallBack)
+
+    if (call.data.startswith("['key'")):
+        keyFromCallBack = ast.literal_eval(call.data)[1]
+        del stringList[keyFromCallBack]
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              text="Here are the values of stringList",
+                              message_id=call.message.message_id,
+                              reply_markup=makeKeyboard(),
+
+                              parse_mode='HTML')
+
