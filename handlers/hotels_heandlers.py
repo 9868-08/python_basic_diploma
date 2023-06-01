a61f5306db3ea869468a3e7635c8e32b9d569b7d
Roman <9868.08@gmail.com>
2023-06-01 15:12:51 +0400

    NameError: name 'message' is not defined

--- handlers/hotels_heandlers.py
+++ handlers/hotels_heandlers.py
@@ -101,87 +101,19 @@ def location_processing(call_button: CallbackQuery):
 
     # TODO Продолжаем диалог
     bot.send_message(chat_id=call_button.from_user.id,
-                     text='Следующий вопрос',
+                     text='Сколько отелей показать?',
                      )
-    bot.set_state(...)
+    bot.set_state(message.from_user.id, MyStates.how_much_hotels)
 
-# from telegram import CallbackQuery
-#
-# from loader import bot
-# from states.bot_states import MyStates
-# # from database import diploma_database
-# import json
-# import jsonpickle
-# from telebot.types import Message
-# from rapidapi.get_info import city_search
-# from keyboard.bot_keyboard import makeKeyboard
-# from keyboard.bot_keyboard import handle_query
-# from rapidapi.get_info import get_request
-#
-#
-# @bot.message_handler(commands=['lowprice', 'highprice', 'bestdeal'])
-# def start_scenario(message: Message):
-#     bot.send_message(chat_id=message.from_user.id,
-#                      text='Введи город поиска'
-#                      )
-#     bot.set_state(message.from_user.id, MyStates.city)
-#
-#
-# @bot.message_handler(commands=['lowprice', 'highprice', 'bestdeal'])
-# def start_highprice(message):
-#     message_dict = json.loads(jsonpickle.encode(message))
-#     data = dict()
-#     data['selected_command'] = message_dict['text']
-#     print(data['selected_command'])
-#     bot.send_message(message.chat.id, data['selected_command'] + ' was selected. \n Hi, write me a city')
-#
-#     # diploma_database.Hotel_search(data['selected_command'])
-#     # request = diploma_database.Hotel_search
-#     bot.set_state(message.from_user.id, MyStates.city, message.chat.id)
-# #    get_checkin.create_calendar(1)
-#     # create_calendar(callback_data, min_date=None, is_process=None, locale='ru'):
-#     return
-#
-#
-# @bot.message_handler(state=MyStates.city)
-# def city_answer(message: Message):
-#
-#     with bot.retrieve_data(message.from_user.id) as data:  # Сохраняем имя города
-#         data['city'] = message.text
-#
-#     cities = city_search(message.text)  # Делаем запрос к API
-#     keyboard = makeKeyboard(cities)  # Формируем клавиатуры
-#
-# # Отправляем пользователю
-#     bot.send_message(chat_id=message.from_user.id,
-#                      text='Уточни выбор:',
-#                      reply_markup=keyboard
-#                      )
-#     bot.set_state(message.from_user.id, MyStates.location_confirmation)
-#
-#
-# @bot.message_handler(state=MyStates.city)
-# def MyStates_city(message):
-#     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-#         data['city'] = message.text
-#
-# #    location_dict = rapidapi.get_info.api_request('locations/v3/search', {"q": data['city'], "locale": "ru_RU"}, 'GET')
-#
-# #    handlers.hotels_heandlers.handle_command_adminwindow(message, location_dict)
-#     bot.set_state(message.from_user.id, MyStates.city_detail, message.chat.id)
-#     # bot.set_state(message.from_user.id, MyStates.how_much_hotels, message.chat.id)
-#     bot.send_message(message.chat.id, 'Now write how much hotels to search')
-#     return
-#
-#
-# @bot.message_handler(state=MyStates.how_much_hotels)
-# def MyStates_how_much_hotels(message):
-#     bot.send_message(message.chat.id, 'Need photos?')
-#     bot.set_state(message.from_user.id, MyStates.need_photos, message.chat.id)
-#     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-#         data['how_much_hotels'] = message.text
-#
-#
+
+@bot.message_handler(state=MyStates.how_much_hotels)
+def MyStates_how_much_hotels(message):
+    bot.send_message(message.chat.id, 'Need photos?')
+    bot.set_state(message.from_user.id, MyStates.need_photos, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['how_much_hotels'] = message.text
+
+
 # @bot.message_handler(state=MyStates.need_photos)
 # def MyStates_need_photos(message):
 #     if message.text == "Y":
