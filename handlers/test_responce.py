0fe4342d9b39467015c75720234a93d58b5fba86
Roman <9868.08@gmail.com>
2023-08-30 16:12:49 +0400

    <Response [204]>

---
+++ handlers/test_responce.py
@@ -0,0 +1,42 @@
+import requests
+
+headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": "f11fe670bamshdaffc29f3f33e2bp19bde9jsn5f0726b4228a",
+    "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+}
+
+payload = {
+    "currency": "USD",
+    "eapid": 1,
+    "locale": "en_US",
+    "siteId": 300000001,
+    "destination": {"regionId": "6054439"},
+    "checkInDate": {
+        "day": 10,
+        "month": 10,
+        "year": 2023
+    },
+    "checkOutDate": {
+        "day": 15,
+        "month": 10,
+        "year": 2023
+    },
+    "rooms": [
+        {
+            "adults": 2,
+            "children": [{"age": 5}, {"age": 7}]
+        }
+    ],
+    "resultsStartingIndex": 0,
+    "resultsSize": 200,
+    "sort": "PRICE_LOW_TO_HIGH",
+    "filters": {"price": {
+        "max": 150,
+        "min": 100
+    }}
+}
+
+response = requests.request("POST", "https://hotels4.p.rapidapi.com/properties/v2/detail", json=payload,
+                            headers=headers)
+print(response, response.text)
