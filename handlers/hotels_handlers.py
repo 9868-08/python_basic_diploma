19ce2209312aacdd220bab5d3e646e815c9e367f
Roman <9868.08@gmail.com>
2024-01-01 19:49:31 +0400

    1) missing 1 required positional argument: 'inline_keyboard'

--- handlers/hotels_handlers.py
+++ handlers/hotels_handlers.py
@@ -3,7 +3,7 @@ from datetime import date
 from telebot.types import Message, CallbackQuery
 from telegram_bot_calendar import DetailedTelegramCalendar
 
-from rapidapi.get_info import api_request
+import rapidapi.rapidapi_payload
 from loader import bot
 from states.bot_states import MyStates
 import jsonpickle
@@ -11,6 +11,7 @@ import json
 from database.bot_database import (history_put, history_list)
 from rapidapi.work_with_city import city_search
 from keyboard.bot_keyboard import city_markup
+from rapidapi.rapidapi_payload import bot_payload
 # import handlers
 ALL_STEPS = {'y': 'год', 'm': 'месяц', 'd': 'день'}  # чтобы русифицировать сообщения
 
@@ -161,65 +162,8 @@ def print_results(message: Message):
                          text='минимальное расстояние от центра, где будем искать'
                          )
         return
-    payload = {
-        "currency": "USD",
-        "eapid": 1,
-        "locale": "en_US",
-        "siteId": 300000001,
-        "destination": {"regionId": "6054439"},
-        "checkInDate": {
-            "day": 10,
-            "month": 10,
-            "year": 2023
-        },
-        "checkOutDate": {
-            "day": 15,
-            "month": 10,
-            "year": 2023
-        },
-        "rooms": [
-            {
-                "adults": 2,
-                "children": [{"age": 5}, {"age": 7}]
-            }
-        ],
-        "resultsStartingIndex": 0,
-        "resultsSize": 200,
-        "sort": bot_sort,
-        "filters": {"price": {
-            "max": 150,
-            "min": 100
-        }}
-    }
-    hotel_id_json = api_request('properties/v2/list', payload, 'POST')
-    parsed_dict = hotel_id_json['data']['propertySearch']
-    # hotel_id_list = []
-    founded_hotel = []
-    count = 1
-    for item in parsed_dict['properties']:
-        if count > int(data['how_much_hotels']):
-            break
-        data['distanceFromDestination'] = item['destinationInfo']['distanceFromDestination']['value']
-        payload = {
-            "currency": "USD",
-            "eapid": 1,
-            "locale": "en_US",
-            "siteId": 300000001,
-            "propertyId": item['id']
-        }  # дефолтые значения с сайта
-        properties_v2_detail_responce = api_request('properties/v2/detail', payload, 'POST')
-        data['address'] = (
-            properties_v2_detail_responce)['data']['propertyInfo']['summary']['location']['address']['addressLine']
-        data['price'] = item['price']['options'][0]['formattedDisplayPrice']
-        hotel_info = 'отель: ' + str(item['name']) + \
-                     '\nадрес: ' + str(data['address']) + \
-                     '\nкак далеко расположен от центра (мили): ' + str(data['distanceFromDestination'])
-        bot.send_message(message.chat.id, str(count) + '\n' + hotel_info)
-        # bot.send_photo(str(item['propertyImage']['image']['url']))
-        bot.send_photo(message.chat.id, str(item['propertyImage']['image']['url']),
-                       caption='фото в отеле ' + item['name'])
-        count += 1
-        founded_hotel.append(str(item['name']))
+    payload, founded_hotel, hotel_id_json = rapidapi.rapidapi_payload.bot_payload(message)
+
     history_put(message.from_user.id, message.from_user.full_name, command=data['selected_command'],
                 result=founded_hotel)
     bot.delete_state(message.from_user.id, message.chat.id)
